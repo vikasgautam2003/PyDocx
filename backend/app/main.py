e164e0ced6ec0cdfb769554c43a8d262eaad3eda
Vikas Gautam <vikasgautamug2003@gmail.com>
2026-02-11 01:31:26 +0530

    made CORS policy enabled in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,9 +26,15 @@ app.include_router(documents.router, prefix="/api/v1", tags=["Documents"])
 app.include_router(chat.router, prefix="/api/v1", tags=["Chat"])
 
 
+origins = [
+    "http://localhost:3000",                   # Local Development   # <--- YOUR VERCEL DOMAIN (Exact URL)
+    "https://docunexusai.vercel.app/",            # (Optional) If you have a custom domain
+    "*"                                        # (Temporary debug) allows EVERYONE.
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
